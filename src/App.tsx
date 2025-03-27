88f02ce161a0355027b2fa170a3b09014e775e37
meytizn <zamanianmehdi23desktop@gmail.com>
2025-03-27 22:02:08 +0330

    app started

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+    <div className='w-[100%] m-auto bg-blue-500 text-[25px] text-white text-center'>quiz appication started </div>
+
+    </>
+  )
+}
+
+export default App
