e030096d2f413e59fdcc171deb6b16b9a940a71a
meytizn <zamanianmehdi23desktop@gmail.com>
2025-03-28 11:21:54 +0330

    useeffect commited

--- src/Questions.tsx
+++ src/Questions.tsx
@@ -1,7 +1,12 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 
 export default function Questions() {
+
+  let [next,Setnext]=useState<number>(0)
+  let [score,setscore]=useState<number>(0)
+
+
   interface QuestionInterface {
     number:string,
     title:string,
@@ -11,33 +16,49 @@ export default function Questions() {
   }
 
   let questions:QuestionInterface[] =[
-    {number:"A",title:"what is js ?" , answer:"js",options:["ts","js","vs","jsx"]},
+    {number:"A",title:"what is js ?" , answer:"javascript",options:["javascript","js","vs","jsx"]},
     {number:"B",title:"what is py ?" , answer:"python",options:["ts","js","vs","python"]},
     {number:"C",title:"what is py ?" , answer:"python",options:["ts","js","vs","python"]},
     {number:"D",title:"what is py ?" , answer:"python",options:["ts","js","vs","python"]},
   ]
 
 
-   let [next,Setnext]=useState<number>(0)
-  //  let [checker,Setchecker]=useState<boolean>(false)
 
 
   function answerhandler(option:string):void{
 
-   
-    if (option==questions[next].answer) {
-      // alert("correct")
-      Setnext(next+=1)
-      // Setchecker(true)
+    Setnext(next+=1)
 
-      
+
+    if(next<questions.length){
+
+
+
+      if (option==questions[next].answer) {
+        alert("correct")
+        setscore(score+=1)
+        }
+        else {      
+          setscore(score-=1)
+        }
+
+
+
     }
-    return (
-      Setnext(next+=1)
-      // Setchecker(false),
-     
-    )
+
+
+
+    else{
+      return (alert(`questions finished and you've scored ${score} scores `),location.reload())
+    }
+
+
+
+
   
+
+
+    
     
 
   }
@@ -49,10 +70,17 @@ export default function Questions() {
 
       <div className=""> {questions[next].number} ) - {questions[next].title}</div>
 
-      <div className="flex flex-col justify-around items-center content-center md:flex-row md:justify-around  content-center  gap-3 ">{questions[next].options.map((Option,index?:number)=><button className={`my-5 p-5 w-[55%] bg-white text-black border `}  onClick={()=>answerhandler(Option)}  key={index}>{Option}</button>)}</div>
+      <div className="flex flex-col justify-around items-center content-center md:flex-row md:justify-around   ">{questions[next].options.map((Option,index?:number)=><button className={`my-3 p-3 w-[55%] bg-white text-black border `}  onClick={()=>answerhandler(Option)}  key={index}>{Option}</button>)}</div>
 
     </div>
 
+
+
+<div className="flex flex-row justify-around w-[95%] m-auto bg-blue-500 text-[25px] text-white my-3 p-5 rounded-md">
+<div>questions :  {next}</div>
+<div>score :  {score}</div>
+<div>len :  {questions.length}</div>
+</div>
     
     </>
   )
